5eb09749f6a6c3ab67f5980b38482d583e787fdf
precioustosin <precioustosin@hotmail.com>
2017-10-17 21:21:25 +0100

    added function to reformat data from server. added code to initialise datatables

--- src/main.js
+++ src/main.js
@@ -4,8 +4,46 @@ import '../public/stylesheets/style.css';
 
 const $ = require('jquery');
 
-$(document).ready(() => {});
+require('datatables.net-bs4');
+require('datatables.net-buttons-bs4');
+// require('datatables.net-fixedheader-bs4')();
+require('datatables.net-responsive-bs4');
 
-const loadData = () => {}
+const reFormat = (data) => {
+  const finalData = {};
+  const largerArr = [];
 
+  data.students.forEach((studentObj) => {
+    const studentArr = $.map(studentObj, el => el.toString());
+    largerArr.push(studentArr);
+  });
+
+  finalData.data = largerArr;
+  console.log(finalData);
+  // console.log(JSON.stringify(finalData));
+  // return JSON.stringify(finalData);
+  return finalData.data;
+};
+
+/*
+const fetchStudents = () => {
+  $.ajax({
+    url: '/students',
+  }).done((data) => {
+    console.log('RETRIEVED DATA');
+    console.log(data);
+    return reFormat(data);
+  });
+};
+*/
+
+$(document).ready(() => {
+  // fetchStudents();
+  $('#my-table').DataTable({
+    ajax: {
+      url: '/students',
+      dataSrc: d => reFormat(d),
+    },
+  });
+});
 
